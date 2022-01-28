ad1d1924a83e00f122bef08916dd8c1e59aa2dd0
aestas <hawhawba@gmail.com>
2022-01-28 14:53:26 +0900

    수정, 삭제 api 생성

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.listen(port, () => {
   console.log(`서버가 실행됩니다. http://localhost:${port}`);
 });
 
-// 클라이언트에서 HTTP 요청 메소드 중 GET을 이용해서 'host:3000/consults'로 요청을 보내면 실행되는 라우트입니다.
+// 클라이언트에서 HTTP 요청 메소드 중 GET을 이용해서 'host:3000/api/consults'로 요청을 보내면 실행되는 라우트입니다.
+// 컨설트 정보 조회 api
 app.get("/api/consults", async (req, res) => {
   // localhost:3000/customers 접속 시 실행
   const consults = await mysql.query("consultList"); //sql.js 파일에 작성된 customerList 쿼리를 실행
@@ -32,8 +33,21 @@ app.get("/api/consults", async (req, res) => {
   res.send(consults); // 결과를 클라이언트로 보냄
 });
 
+// 새로운 컨설트 정보 추가 api
 app.post("/api/consult/insert", async (req, res) => {
   console.log(req.body);
   const result = await mysql.query("consultInsert", req.body.param);
+  res.send(result);
+});
+// put 컨설트 정보 수정 api
+app.put("/api/consult/update", async (req, res) => {
+  const result = await mysql.query("consultUpdate", req.body.param);
+  res.send(result);
+});
+
+// delete 컨설트 정보 수정 api
+app.delete("/api/consult/delete/:id", async (req, res) => {
+  const { id } = req.params; // 라우트 경로의 :id 에 매핑되는 값
+  const result = await mysql.query("consultDelete", id);
   res.send(result);
 });
--- mysql/sql.js
+++ mysql/sql.js
@@ -1,4 +1,6 @@
 module.exports = {
   consultList: `select * from consults order by datetime desc`,
   consultInsert: `insert into consults set ?`,
+  consultUpdate: `update consults set ? where id=?`,
+  consultDelete: `delete from consults where id=?`,
 };
